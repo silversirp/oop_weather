93d2b3a78569e232d3487580d4330cb71299d40d
Silver Sirp <silver.sirp@khk.ee>
2021-12-02 11:43:41 +0200

    Set and get ls city data

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// LS object
+const ls = new LS()
+// init caity data
+const initCity = ls.getCity()
+
 // Weather object
-const weather = new Weather('Tallinn')
+const weather = new Weather(initCity)
 // UI object
 const ui = new UI()
 
@@ -11,6 +16,7 @@ form.addEventListener('submit', changeWeather)
 function changeWeather(event){
     const city = document.querySelector('#city-name').value
     weather.changeCity(city)
+    ls.setCity(city)
     getWeather()
     document.querySelector('#city-name').value = ''
     event.preventDefault()
---
+++ classes/LS.js
@@ -0,0 +1,20 @@
+class LS {
+    constructor() {
+        this.defaultCity = 'Tallinn'
+        this.city
+    }
+
+    getCity(){
+        if(localStorage.getItem('city') === null){
+            this.city = this.defaultCity
+        } else {
+            this.city = localStorage.getItem('city')
+        }
+        return this.city
+    }
+
+    setCity(city){
+        localStorage.setItem('city', city)
+    }
+
+}

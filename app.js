f0d5817e1a91eec43da0ca8ba55c68880783d440
Silver Sirp <silver.sirp@khk.ee>
2021-12-02 10:57:33 +0200

    Added get weather data

---
+++ app.js
@@ -0,0 +1,14 @@
+// Weather object
+const weather = new Weather('Tallinn')
+
+
+// get city weather
+function getWeather(){
+    weather.getWeather()
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => console.log(error))
+}
+
+getWeather()
---
+++ classes/Weather.js
@@ -0,0 +1,12 @@
+class Weather{
+    constructor(city) {
+        this.city = city
+        this.key = 'ee91cb438af3ec838d3c334f3d56c61c'
+    }
+
+    async getWeather(){
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responseData = await response.json()
+        return responseData
+    }
+}
